dd0136583b5829e835c6fb23c57716825e091be3
Gísli Kristjánsson <gislik@hamstur.is>
2023-02-27 21:46:06 +0000

    feat: message response

--- src/message.rs
+++ src/message.rs
@@ -5,35 +5,63 @@ use serde::Serialize;
 
 /// Messages are sent from clients to relays. Defined in
 /// [NIP-01](https://github.com/nostr-protocol/nips/blob/master/01.md).
-pub enum Message {
+pub enum MessageRequest {
     Event(Event),
     Request(String, Request),
     Close(String),
 }
 
-impl Serialize for Message {
+impl Serialize for MessageRequest {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer,
     {
         match self {
-            Message::Event(event) => {
+            MessageRequest::Event(event) => {
                 let mut seq = serializer.serialize_seq(Some(2))?;
                 seq.serialize_element(&"EVENT".to_string())?;
                 seq.serialize_element(event)?;
                 seq.end()
             }
-            Message::Request(subscription_id, request) => {
+            MessageRequest::Request(subscription_id, request) => {
                 let mut seq = serializer.serialize_seq(Some(3))?;
                 seq.serialize_element(&"REQ".to_string())?;
                 seq.serialize_element(subscription_id)?;
                 seq.serialize_element(request)?;
                 seq.end()
             }
-            Message::Close(subscription_id) => {
+            MessageRequest::Close(subscription_id) => {
                 let mut seq = serializer.serialize_seq(Some(2))?;
                 seq.serialize_element(&"CLOSE".to_string())?;
                 seq.serialize_element(subscription_id)?;
+                seq.end()
+            }
+        }
+    }
+}
+
+pub enum MessageResponse {
+    Event(String, Event),
+    Notice(String),
+}
+
+impl Serialize for MessageResponse {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        match self {
+            MessageResponse::Event(subscription_id, event) => {
+                let mut seq = serializer.serialize_seq(Some(3))?;
+                seq.serialize_element(&"EVENT".to_string())?;
+                seq.serialize_element(&subscription_id)?;
+                seq.serialize_element(event)?;
+                seq.end()
+            }
+            MessageResponse::Notice(message) => {
+                let mut seq = serializer.serialize_seq(Some(2))?;
+                seq.serialize_element(&"NOTICE".to_string())?;
+                seq.serialize_element(message)?;
                 seq.end()
             }
         }
@@ -48,9 +76,9 @@ mod tests {
     use serde_json::to_string;
 
     #[test]
-    fn serialize_event_works() -> serde_json::Result<()> {
+    fn serialize_event_request_works() -> serde_json::Result<()> {
         let event = event::tests::get_simple_event();
-        let message = Message::Event(event);
+        let message = MessageRequest::Event(event);
         let got = to_string(&message)?;
         let json = event::tests::get_json();
         let want = format!(r#"["EVENT",{}]"#, json);
@@ -59,10 +87,10 @@ mod tests {
     }
 
     #[test]
-    fn serialize_request_works() -> serde_json::Result<()> {
+    fn serialize_request_request_works() -> serde_json::Result<()> {
         let id = "subid".to_string();
         let request = request::tests::get_simple_request();
-        let message = Message::Request(id.clone(), request);
+        let message = MessageRequest::Request(id.clone(), request);
         let got = to_string(&message)?;
         let json = request::tests::get_json();
         let want = format!(r#"["REQ","{}",{}]"#, id, json);
@@ -71,10 +99,32 @@ mod tests {
     }
 
     #[test]
-    fn serialize_close_works() -> serde_json::Result<()> {
-        let message = Message::Close("subid".to_string());
+    fn serialize_close_request_works() -> serde_json::Result<()> {
+        let message = MessageRequest::Close("subid".to_string());
         let got = to_string(&message)?;
         let want = r#"["CLOSE","subid"]"#;
+        assert_eq!(got, want);
+        Ok(())
+    }
+
+    #[test]
+    fn serialize_event_response_works() -> serde_json::Result<()> {
+        let event = event::tests::get_simple_event();
+        let subscription_id = "subid".to_string();
+        let message = MessageResponse::Event(subscription_id, event);
+        let got = to_string(&message)?;
+        let json = event::tests::get_json();
+        let want = format!(r#"["EVENT","subid",{}]"#, json);
+        assert_eq!(got, want);
+        Ok(())
+    }
+
+    #[test]
+    fn serialize_notice_response_works() -> serde_json::Result<()> {
+        let notice = "notice".to_string();
+        let message = MessageResponse::Notice(notice);
+        let got = to_string(&message)?;
+        let want = r#"["NOTICE","notice"]"#;
         assert_eq!(got, want);
         Ok(())
     }
